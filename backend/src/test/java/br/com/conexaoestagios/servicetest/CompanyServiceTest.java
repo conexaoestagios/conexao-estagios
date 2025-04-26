cfbc3a2e2689f40508e5faa53fdc34beb320709c
candido <doug.candido2704@gmail.com>
2025-04-26 16:21:40 -0300

    test: testes alterados para nova arquitetura

--- backend/src/test/java/br/com/conexaoestagios/servicetest/CompanyServiceTest.java
+++ backend/src/test/java/br/com/conexaoestagios/servicetest/CompanyServiceTest.java
@@ -1,10 +1,16 @@
 package br.com.conexaoestagios.servicetest;
 
+import br.com.conexaoestagios.dto.address.AddressRequestDTO;
 import br.com.conexaoestagios.dto.company.CompanyRequestDTO;
 import br.com.conexaoestagios.dto.company.CompanyResponseDTO;
-import br.com.conexaoestagios.dto.company.CompanyUpdateDTO;
+import br.com.conexaoestagios.dto.user.UserRequestDTO;
 import br.com.conexaoestagios.entities.Company;
+import br.com.conexaoestagios.entities.users.User;
+import br.com.conexaoestagios.enums.Role;
 import br.com.conexaoestagios.exceptions.NoUserException;
+import br.com.conexaoestagios.exceptions.UniqueFieldViolationException;
+import br.com.conexaoestagios.mapper.CompanyMapper;
+import br.com.conexaoestagios.mapper.UserMapper;
 import br.com.conexaoestagios.repository.CompanyRepository;
 import br.com.conexaoestagios.service.CompanyService;
 import br.com.conexaoestagios.service.UserService;
@@ -13,9 +19,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,126 +31,152 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 public class CompanyServiceTest {
-//
-//    @InjectMocks
-//    private CompanyService companyService;
-//
-//    @Mock
-//    private Company company;
-//    @Mock
-//    private CompanyRequestDTO companyRequestDTO;
-//    @Mock
-//    private CompanyRepository companyRepository;
-//    @Mock
-//    private PasswordEncoder passwordEncoder;
-//    @Mock
-//    private UserService userService;
-//
-//
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//
-//        company = new Company();
-//        company.setId(1L);
-//        company.setName("Tech Corp");
-//        company.setUsername("techcorp");
-//        company.setEmail("contact@techcorp.com");
-//        company.setPassword("password123");
-//        company.setSector("TI");
-//        company.setPhoneNumber("11999999999");
-//        company.setRegistrationDate(LocalDateTime.now());
-//
-//        companyRequestDTO = new CompanyRequestDTO(
-//                company.getName(), company.getUsername(), company.getEmail(),
-//                company.getPassword(), company.getSector(),
-//                company.getPhoneNumber(), company.getCity(), company.getState());
-//    }
-//
-//    @Test
-//    void should_Create_Company() {
-//        when(companyRepository.save(any(Company.class))).thenReturn(company);
-//        when(passwordEncoder.encode(anyString())).thenReturn("hashedPassword123");
-//
-//        CompanyResponseDTO createdCompany = companyService.create(companyRequestDTO);
-//
-//        assertNotNull(createdCompany);
-//        assertEquals(company.getName(), createdCompany.name());
-//        assertEquals(company.getEmail(), createdCompany.email());
-//        assertEquals(company.getRole(), createdCompany.role());
-//        verify(passwordEncoder, times(1)).encode(companyRequestDTO.password());
-//        verify(companyRepository, times(1)).save(any(Company.class));
-//    }
-//
-//    @Test
-//    void should_throw_ConflictException() {
-//        doThrow(new ConflictException("nome de usuário já está em uso."))
-//                .when(userService).verifyIfUsernameIsUsed(companyRequestDTO.username());
-//        doThrow(new ConflictException("email já está em uso."))
-//                .when(userService).verifyIfEmailIsUsed(companyRequestDTO.email());
-//        assertThrows(ConflictException.class, () -> companyService.create(companyRequestDTO));
-//    }
-//
-//    @Test
-//    void should_FindCompanyByID() {
-//        when(companyRepository.findById(1L)).thenReturn(Optional.of(company));
-//
-//        Optional<CompanyResponseDTO> foundCompany = Optional.of(companyService.findById(1L));
-//
-//        assertNotNull(foundCompany);
-//        assertEquals(company.getId(), foundCompany.get().id());
-//        assertEquals(company.getName(), foundCompany.get().name());
-//        assertEquals(company.getRole(), foundCompany.get().role());
-//        verify(companyRepository, times(1)).findById(1L);
-//    }
-//
-//    @Test
-//    void should_throw_NoCompanyException() {
-//        when(companyRepository.findById(1L)).thenReturn(Optional.empty());
-//
-//        assertThrows(NoUserException.class, () -> companyService.findById(1L));
-//    }
-//
-//    @Test
-//    void should_ReturnAllCompanies() {
-//        Company anotherCompany = new Company();
-//        anotherCompany.setId(2L);
-//        anotherCompany.setName("Another Corp");
-//        anotherCompany.setEmail("another@example.com");
-//        anotherCompany.setPassword("password1234");
-//        anotherCompany.setPhoneNumber("11988888888");
-//        anotherCompany.setRegistrationDate(LocalDateTime.now());
-//
-//        when(companyRepository.findAll()).thenReturn(List.of(company, anotherCompany));
-//
-//        List<CompanyResponseDTO> companies = companyService.findAll();
-//
-//        assertNotNull(companies);
-//        assertEquals(2, companies.size());
-//        verify(companyRepository, times(1)).findAll();
-//    }
-//
-//    @Test
-//    void should_UpdateCompany() {
-//        when(companyRepository.findById(1L)).thenReturn(Optional.of(company));
-//        when(companyRepository.save(company)).thenReturn(company);
-//
-//        company.setName("Updated Name");
-//        CompanyUpdateDTO companyUpdateDTO =
-//                new CompanyUpdateDTO(company.getName(), company.getUsername(), company.getEmail(), company.getSector(), company.getPhoneNumber(), company.getCity(), company.getState());
-//
-//        CompanyResponseDTO updatedCompany = companyService.update(1L, companyUpdateDTO);
-//
-//        assertNotNull(updatedCompany);
-//        assertEquals("Updated Name", updatedCompany.name());
-//        verify(companyRepository, times(1)).findById(1L);
-//        verify(companyRepository, times(1)).save(company);
-//    }
-//
-//    @Test
-//    void should_DeleteCompany() {
-//        Long id = 1L;
-//        when(companyRepository.existsById(id)).thenReturn(true);
-//        when(companyRepository.getReferenceById(id)).thenReturn(company);
-//    }
+
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Mock
+    private CompanyRepository companyRepository;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private UserService userService;
+
+    private Company company;
+    private User user;
+    private AddressRequestDTO addressRequestDTO;
+    private UserRequestDTO userRequestDTO;
+    private CompanyRequestDTO companyRequestDTO;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        addressRequestDTO = new AddressRequestDTO("58057000", "50", "B", "Azuza", "Mangabeira",
+                "João Pessoa", "Paraíba", "Brazil");
+
+        userRequestDTO = new UserRequestDTO("Tech Corp", "techcorp", "contact@techcorp.com",
+                "password123", "11999999999", "http://www.linkedin.com/in/techcorp", addressRequestDTO);
+
+        user = UserMapper.toEntity(userRequestDTO, Role.EMPRESA);
+
+        companyRequestDTO = new CompanyRequestDTO("Tech Corp Ltda", "12345678000190", "Tecnologia", userRequestDTO);
+        company = CompanyMapper.toEntity(companyRequestDTO, user);
+        company.setId(1L);
+    }
+
+    @Test
+    void should_Create_Company() {
+        when(passwordEncoder.encode(anyString())).thenReturn("hashedPassword123");
+
+        // Mockando o retorno do userService.create para não retornar null
+        when(userService.create(any(UserRequestDTO.class), eq(Role.EMPRESA))).thenReturn(user);
+
+        // Mockando o retorno do companyRepository.save
+        when(companyRepository.save(any(Company.class))).thenReturn(company);
+
+        CompanyResponseDTO createdCompany = companyService.create(companyRequestDTO);
+
+        assertNotNull(createdCompany);
+        assertEquals(company.getUser().getName(), createdCompany.userResponseDto().name());
+        assertEquals(company.getUser().getEmail(), createdCompany.userResponseDto().email());
+        assertEquals(company.getUser().getRole(), createdCompany.userResponseDto().role());
+
+        verify(companyRepository, times(1)).save(any(Company.class));
+    }
+
+
+    @Test
+    void should_throw_UniqueFieldViolationException() {
+        doThrow(new UniqueFieldViolationException(new DataIntegrityViolationException("email já em uso")))
+                .when(userService).create(any(UserRequestDTO.class), eq(Role.EMPRESA));
+
+        assertThrows(UniqueFieldViolationException.class, () -> companyService.create(companyRequestDTO));
+    }
+
+    @Test
+    void should_FindCompanyByID() {
+        when(companyRepository.findById(1L)).thenReturn(Optional.of(company));
+
+        CompanyResponseDTO foundCompany = companyService.findById(1L);
+
+        assertNotNull(foundCompany);
+        assertEquals(company.getId(), foundCompany.id());
+        assertEquals(company.getUser().getName(), foundCompany.userResponseDto().name());
+        assertEquals(company.getUser().getRole(), foundCompany.userResponseDto().role());
+        verify(companyRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    void should_throw_NoCompanyException() {
+        when(companyRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(NoUserException.class, () -> companyService.findById(1L));
+    }
+
+    @Test
+    void should_ReturnAllCompanies() {
+        User anotherUser = UserMapper.toEntity(userRequestDTO, Role.EMPRESA);
+        Company anotherCompany = CompanyMapper.toEntity(companyRequestDTO, anotherUser);
+        anotherCompany.setId(2L);
+
+        when(companyRepository.findAll()).thenReturn(List.of(company, anotherCompany));
+
+        List<CompanyResponseDTO> companies = companyService.findAll();
+
+        assertNotNull(companies);
+        assertEquals(2, companies.size());
+        verify(companyRepository, times(1)).findAll();
+    }
+
+    // TODO: implementar lógica real de atualização para que esse teste funcione corretamente
+    @Test
+    void should_UpdateCompany() {
+
+        User mockUser = new User();
+        mockUser.setId(1L);
+        mockUser.setName("Empresa Original");
+        mockUser.setEmail("empresa@email.com");
+        mockUser.setRole(Role.EMPRESA);
+
+        company = new Company();
+        company.setId(1L);
+        company.setCnpj("12345678000100");
+        company.setLegalName("Empresa Original");
+        company.setSector("Tecnologia");
+        company.setUser(mockUser);
+
+        CompanyRequestDTO companyRequestDTO1 = new CompanyRequestDTO(
+                "12345678000199", "Empresa Atualizada", "Educação", // novo setor
+                new UserRequestDTO("Empresa Atualizada", "novo@email.com", "12345678900",
+                        "novaSenha", null, null, null));
+
+        when(companyRepository.findById(1L)).thenReturn(Optional.of(company));
+        when(companyRepository.save(any(Company.class))).thenReturn(company);
+        when(userService.update(eq(1L), any(UserRequestDTO.class))).thenReturn(mockUser); // simula o update do usuário
+
+        CompanyResponseDTO response = companyService.update(1L, companyRequestDTO1);
+
+        assertNotNull(response);
+        assertEquals("Empresa Atualizada", response.userResponseDto().name());
+        assertEquals("Educação", response.sector());
+
+        verify(companyRepository, times(1)).findById(1L);
+        verify(companyRepository, times(1)).save(any(Company.class));
+        verify(userService, times(1)).update(eq(1L), any(UserRequestDTO.class));
+    }
+
+
+    @Test
+    void should_DeleteCompany() {
+        Long id = 1L;
+        when(companyRepository.existsById(id)).thenReturn(true);
+        when(companyRepository.getReferenceById(id)).thenReturn(company);
+
+        companyService.delete(id);
+
+        verify(companyRepository, times(1)).existsById(id);
+        verify(companyRepository, times(1)).getReferenceById(id);
+        verify(companyRepository, times(1)).delete(company);
+    }
 }

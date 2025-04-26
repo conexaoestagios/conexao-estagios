d1aa1bd65deb0e18c4b05e22b8d9b206c701edab
candido <doug.candido2704@gmail.com>
2025-04-26 16:33:49 -0300

    refactor: exceções alteradas para nova arquitetura

--- backend/src/main/java/br/com/conexaoestagios/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/br/com/conexaoestagios/exceptions/GlobalExceptionHandler.java
@@ -6,28 +6,25 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoUserException.class)
-    public ResponseEntity<String> handleNoUserException(NoUserException ex, WebRequest request) {
+    public ResponseEntity<String> handleNoUserException(NoUserException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
     @ExceptionHandler(NoPermissionUserException.class)
-    public ResponseEntity<String> handleNoPermissionException(NoPermissionUserException ex, WebRequest request) {
+    public ResponseEntity<String> handleNoPermissionException(NoPermissionUserException ex) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAllExceptions(Exception ex, WebRequest request) {
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno: " + ex.getMessage());
     }
 
@@ -42,10 +39,8 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(UniqueFieldViolationException.class)
-    public ResponseEntity<Map<String, List<String>>> handleUniqueViolation(UniqueFieldViolationException ex) {
-        Map<String, List<String>> response = new HashMap<>();
-        response.put("erros", ex.getErrors());
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
+    public ResponseEntity<List<String>> handleUniqueViolation(UniqueFieldViolationException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessages());
     }
 
 }
